857fb706b5890230b69b36d6ab7fb85a7a24030a
melodytowett <melodytowett99@gmail.com>
2022-03-11 13:59:04 +0300

    istall migration and creating first migration

--- app/models.py
+++ app/models.py
@@ -1,6 +1,15 @@
+from .import db
+
 class Quotes:
     def __init__(self,author,id,quote):
         self.id=id
         self.author=author
         self.quote=quote
-        
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User{self.username}'
