83a543bab3f3e3b87dd2bcfd8cebbf3142ca8d22
melodytowett <melodytowett99@gmail.com>
2022-03-11 12:26:02 +0300

    fetching the quotes

--- app/request.py
+++ app/request.py
@@ -9,10 +9,11 @@ def get_quotes():
     # with urllib.request.urlopen(get_quotes_url)as url:
     get_quotes_data = urllib.request.urlopen(get_quotes_url)
     get_quotes_response = json.loads(get_quotes_data.read())
-
-    id= get_quotes_response.get('id')
-    author = get_quotes_response.get('author')
-    quote= get_quotes_response.get('quotes')
+    new_quote = None
+    if get_quotes_response:
+       id= get_quotes_response.get('id')
+       author = get_quotes_response.get('author')
+       quote= get_quotes_response.get('quote')
 
     new_quote = Quotes(author,id,quote)
 
