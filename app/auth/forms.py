fde41a38a93fac8cabf8246a9d658410f4e32a75
melodytowett <melodytowett99@gmail.com>
2022-03-11 17:25:14 +0300

    add login form and route

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,9 @@
 
+from ast import Str
 from importlib_metadata import email
 from . import views,forms
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
 
@@ -10,5 +11,11 @@ class RegistrationForm(FlaskForm):
     email = StringField('Enter your email address', validators=[DataRequired(),Email()])
     username = StringField('Enter your name',validators=[DataRequired()])
     password = PasswordField('password',validators=[DataRequired(),EqualTo('password_confirm',message = 'passwords must match')])
-    password_confirm = PasswordField('Confiem password',validators=[DataRequired()])
+    password_confirm = PasswordField('Confirm password',validators=[DataRequired()])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField("Email Address", validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField('Login')
