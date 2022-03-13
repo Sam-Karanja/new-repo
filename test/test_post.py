164e94dd1830ecf1f6346828ba350d4cbe388301
melodytowett <melodytowett99@gmail.com>
2022-03-13 21:53:57 +0300

    try display in cards

--- test/test_post.py
+++ test/test_post.py
@@ -1,4 +1,5 @@
-from app.models import Post,User
+
+from app.models import Post,User,Comment
 from app import db
 import unittest
 
@@ -7,17 +8,32 @@ class PosTest(unittest.TestCase):
     Creating n instance of User and pitch and pass it
     '''
     def setUp(self):
-        self.user_Melo = User(username = 'melo',password='1234',email = 'melo@gmail.com')
-        self.new_post = Post( body='input your personal blog here',user = self.user_Melo)
+        self.new_post = Post( body='input your personal blog here')
 
     def tearDown(self):
         Post.query.delete()
-        User.query.delete()
+  
 
     def test_check_instance_variable(self):
         self.assertEquals(self.new_post.body,'input your personal blog here')
-        self.assertEquals(self.new_post.user,self.user_Melo)
 
     def test_save_post(self):
-        self.new_pitch.save_post()
+        self.new_post.save_post()
         self.assertTrue(len(Post.query.all())>0)
+
+class CommentTest(unittest.TestCase):
+    def setUp(self):
+        self.new_comment = Comment(comment = 'addyour comment here')
+
+    def tearDown(self):
+        Comment.query.delete()
+
+    def test_check_instance_variable(self):
+        self.assertAlmostEquals(self.new_comment,'addyour comment here')
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
+
+
+       
